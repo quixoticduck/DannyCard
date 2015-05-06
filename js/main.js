07b98dc77e49ec8ddae0bc02a5d6d8d81fb51450
Clara Alder <claraalder@Claras-MacBook.local>
2015-05-06 01:10:51 +0100

    added Danny's birthday code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
 
-setTimeout(function(){ 
-$('.h1').textillate({ in: { effect: 'rollIn' } });
-}, 3000);
+	setTimeout(function(){ 
+	$('.h1').textillate({ in: { effect: 'rollIn' } });
+	}, 3000);
 
 	$( ".btn-name-elephant" ).click(function() {
-		$("h1").html("This is " + $(".name-input").val() );
+		$("h1").html("This is " + $(".name-input").val() + " the elephant." );
 	});
 
 	$( ".btn-party-hat" ).click(function() {
@@ -25,4 +25,20 @@ $('.h1').textillate({ in: { effect: 'rollIn' } });
 		$("h1").html("It is " + $(".name-input").val() );
 	});
 
+	$("#age").text(stringAge);
+	console.log(stringAge)
 });
+
+var d = new Date();
+var b = new Date (1991,3,12);
+var day = 86400000;
+var year = day * 365;
+var yearsOld = (d - b)/year;
+console.log(yearsOld);
+var daysOld = (d - b)/day;
+console.log(daysOld);
+
+var yearsRounded = Math.floor(yearsOld);
+var daysRounded = Math.floor(daysOld % 365);
+
+var stringAge = "You are: " + yearsRounded + " years and " + daysRounded + " days old.";
